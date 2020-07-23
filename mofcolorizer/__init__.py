34414a48e5d0fdaa3f3a134c1c91a344d166f291
Kevin <kevin.jablonka@epfl.ch>
2020-07-23 08:49:17 +0200

    feat: various small improvements

--- mofcolorizer/__init__.py
+++ mofcolorizer/__init__.py
@@ -5,16 +5,17 @@ from __future__ import absolute_import, print_function
 
 import logging
 
-import crystal_toolkit.components as ctc
 import dash
-import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 from flask import session
-from flask_session import Session
 from pymatgen import Lattice, Structure
+
+import crystal_toolkit.components as ctc
+import dash_bootstrap_components as dbc
+from flask_session import Session
 
 from . import dash_reusable_components as drc
 from .core import predict
